3a1c7438bf96309673626bbfe1b49c04fa2c50bf
Daniel Serrano <dserranog@unal.edu.co>
2023-05-03 11:28:51 -0500

    create table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TableComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/table/table.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
+
+export interface UserRequest {
+  name: string;
+  subject: string;
+  customerSatisfaction: number;
+  priority: 'High' | 'Medium' | 'Low';
+  status: 'Solved' | 'Open' | 'Pending';
+}
+
+function createRandomRequests(n: number) {
+  let requests: UserRequest[] = [];
+  for (let i = 0; i < n; i++) {
+    requests.push(createSingleRequest());
+  }
+  return requests;
+}
+
+function createSingleRequest(): UserRequest {
+  return {
+    name: faker.name.fullName(),
+    subject: faker.lorem.sentence(),
+    customerSatisfaction: faker.datatype.number({ min: 0, max: 100 }),
+    priority: faker.helpers.arrayElement(['High', 'Medium', 'Low']),
+    status: faker.helpers.arrayElement(['Solved', 'Open', 'Pending']),
+  };
+}
+
+const ELEMENT_DATA: UserRequest[] = createRandomRequests(200);
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss'],
+})
+export class TableComponent {
+  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = [
+    'userName',
+    'subject',
+    'customerSatisfaction',
+    'priority',
+    'status',
+  ];
+}
